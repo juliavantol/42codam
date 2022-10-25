811dd9f0ca40c45d0f8eba32a6748afcc241c591
Julia van Tol <juliavantol@live.nl>
2022-10-25 12:31:37 +0200

    add home

--- codamcore/rank0/libft/ft_atoi.c
+++ codamcore/rank0/libft/ft_atoi.c
@@ -6,31 +6,56 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/13 13:03:01 by juvan-to      #+#    #+#                 */
-/*   Updated: 2022/10/24 18:33:18 by juvan-to      ########   odam.nl         */
+/*   Updated: 2022/10/25 12:29:46 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /* This function converts the initial portion of str to int */
+static int	convert(const char *str, int index, int n);
+static int	check_start(const char *str);
 
-static int check_start(const char *str);
-
-static int check_start(const char *str)
+static int	check_start(const char *str)
 {
 	int	start;
+	int	n;
 
 	start = 0;
-	while (!ft_isdigit(str[start]))
+	n = 1;
+	while ((str[start] >= 9 && str[start] <= 13) || str[start] == ' ')
 		start++;
-	return (start);
+	if (str[start] == '-' || str[0] == '-')
+	{
+		n = -1;
+		start++;
+	}
+	else if (str[start] == '+' || str[0] == '+')
+		start++;
+	return (convert(str, start, n));
+}
+
+static int	convert(const char *str, int index, int n)
+{
+	int	sum;
+
+	sum = 0;
+	while (ft_isdigit(str[index]))
+		sum = sum * 10 + str[index++] - 48;
+	return (sum * n);
 }
 
 int	ft_atoi(const char *str)
 {
-	printf("%d\n", check_start(str));
-	return (0);
+	int	sum;
+
+	sum = 0;
+	if (str == NULL || ft_strlen(str) == 0)
+		return (0);
+	sum = check_start(str);
+	return (sum);
 }
+
 
 // int	ft_atoi(const char *str)
 // {
--- codamcore/rank0/libft/main.c
+++ codamcore/rank0/libft/main.c
@@ -6,15 +6,24 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/14 13:35:40 by juvan-to      #+#    #+#                 */
-/*   Updated: 2022/10/24 18:30:59 by juvan-to      ########   odam.nl         */
+/*   Updated: 2022/10/25 12:28:55 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	test_atoi(void)
+{
+	printf("Answer: %d\n", atoi("\t\n\r\v\f  -469 \n"));
+	printf("Mine: %d\n", ft_atoi("\t\n\r\v\f  -469 \n"));
+	printf("Answer: %d\n", atoi("+123"));
+	printf("Mine: %d\n", ft_atoi("+123"));
+}
+
+
 int	main(void)
 {
-	printf("%d\n", ft_atoi("120ahdh9"));
+	test_atoi();
 }
 
 // char	*ft_itoa(int n)
