63e38a924745c6384df6c09c591b24c18df9fcdc
Julia van Tol <juliavantol@live.nl>
2022-11-03 16:08:35 +0100

    add

--- codamcore/rank0/libft/ft_substr.c
+++ codamcore/rank0/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/18 13:36:07 by juvan-to      #+#    #+#                 */
-/*   Updated: 2022/10/31 17:09:25 by juvan-to      ########   odam.nl         */
+/*   Updated: 2022/11/03 16:07:58 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,20 @@ The substring begins at index ’start’ and is of maximum size ’len’. */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	index;
+	size_t	j;
 	char	*sub;
 
 	index = 0;
+	j = 0;
+	if (ft_strlen(s) < len || (start + len) > ft_strlen(s))
+		len = ft_strlen(s) - start;
 	if (start >= ft_strlen(s) || ft_strlen(s) == 0)
 		return (ft_strdup(""));
-	if (len > ft_strlen(s) || len < 0)
-		sub = (char *)malloc((ft_strlen(s) - start + 1) * sizeof(char));
-	else if ((start + len) > ft_strlen(s))
-		sub = (char *)malloc(len * sizeof(char));
-	else
-		sub = (char *)malloc((len + 1) * sizeof(char));
-	if (sub == NULL)
+	sub = (char *)malloc(sizeof(*s) * (len + 1));
+	if (!sub)
 		return (NULL);
-	while (index < len && s[start] != '\0')
-		sub[index++] = s[start++];
-	sub[index] = '\0';
+	while (s[start] && j < len)
+		sub[j++] = s[start++];
+	sub[j] = '\0';
 	return (sub);
 }
