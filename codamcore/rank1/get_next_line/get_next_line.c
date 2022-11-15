e84e3d1b3febb7e3135913f2d6ea86858d961067
Julia van Tol <juliavantol@live.nl>
2022-11-15 17:56:24 +0100

    get next line progress

--- codamcore/rank1/get_next_line/get_next_line.c
+++ codamcore/rank1/get_next_line/get_next_line.c
@@ -6,37 +6,40 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/14 17:40:27 by juvan-to      #+#    #+#                 */
-/*   Updated: 2022/11/14 23:25:02 by Julia         ########   odam.nl         */
+/*   Updated: 2022/11/15 17:53:48 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// cc -Wall -Wextra -Werror -D BUFFER_SIZE=42
+// cc -Wall -Wextra -Werror -D BUFFER_SIZE=42 get_next_line.c &&./a.out
+// cc -Wall -Wextra -Werror get_next_line.c &&./a.out
+
+#ifndef BUFFER_SIZE
+
+# define BUFFER_SIZE 20
+
+#endif
 
 char	*get_next_line(int fd)
 {
-	static int	buffer_size;
-	char		*s;
+	char	s;
 
-#ifdef BUFFER_SIZE
-	buffer_size = BUFFER_SIZE;
-	printf("%d\n", buffer_size);
-
-#else
-
-	buffer_size = 10;
-	printf("no buffer given\n");
-	printf("%d\n", buffer_size);
-#endif
-
-	s = "a";
-	fd--;
-	return (s);
+	s = 'x';
+	while (read(fd, &s, 1))
+	{
+		printf("%c", s);
+		s++;
+	}
+	return (0);
 }
 
 int	main(void)
 {
-	get_next_line(1);
+	int		file;
+
+	file = open("test.txt", O_RDONLY);
+	get_next_line(file);
+	close(file);
 	return (0);
 }
--- codamcore/rank1/get_next_line/get_next_line.h
+++ codamcore/rank1/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/14 17:16:21 by juvan-to      #+#    #+#                 */
-/*   Updated: 2022/11/14 17:19:40 by juvan-to      ########   odam.nl         */
+/*   Updated: 2022/11/15 17:42:53 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 
