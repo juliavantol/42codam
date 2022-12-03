2095ab32f6344821515afb795af6beb344b8e850
Julia van Tol <juliavantol@live.nl>
2022-12-03 11:50:37 +0100

    finds dup

--- advent2022/day3/part1.py
+++ advent2022/day3/part1.py
@@ -1,15 +1,20 @@
 def split_str(line):
 	str_len = len(line)
-	s1 = slice(0, str_len//2)
-	s2 = slice(str_len//2, str_len)
-	list1 = line[s1]
-	list2 = line[s2]
-	output = [list1, list2]
-	return output
+	list1 = line[slice(0, str_len//2)]
+	list2 = line[slice(str_len//2, str_len)]
+	return [list1, list2]
+
+def search(left, right):
+	for i in left:
+		for j in right:
+			if i == j:
+				return (i)
 
 with open("test.txt") as file:
 	while True:
 		line = file.readline()
 		if not line:
 			break
-		print(split_str(line))
+		output = split_str(line)
+		duplicate = search(output[0], output[1])
+		print(duplicate)
