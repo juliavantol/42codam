66af75945fd737ccc8739dbfdfcb301031b00e2f
Julia van Tol <juliavantol@live.nl>
2023-02-09 12:27:27 +0100

    add all

---
+++ practice/inter/inter.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   inter.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/09 12:21:52 by juvan-to      #+#    #+#                 */
+/*   Updated: 2023/02/09 12:27:05 by juvan-to      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	used[255];
+	int	i, j;
+
+	if (argc == 3)
+	{
+		i = 0;
+		while (i < 255)
+			used[i++] = 0;
+		i = 2;
+		while (i > 0)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				if (i == 2 && !used[(unsigned char)argv[i][j]])
+					used[(unsigned char)argv[i][j]] = 1;
+				else if (i == 1 && used[(unsigned char)argv[i][j]] == 1)
+				{
+					write(1, &argv[i][j], 1);
+					used[(unsigned char)argv[i][j]] = 2;
+				}
+				j++;
+			}
+			i--;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
