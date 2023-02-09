446bd2405e2a91fc3d2b9760aa4fc4090a3d3d03
Julia van Tol <juliavantol@live.nl>
2023-02-09 12:39:25 +0100

    ss

--- practice/inter/inter.c
+++ practice/inter/inter.c
@@ -6,12 +6,18 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/09 12:21:52 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/02/09 12:27:05 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/02/09 12:39:04 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+void leaks(void)
+{
+	system ("leaks a.out");
+}
 
 int	main(int argc, char **argv)
 {
@@ -42,5 +48,6 @@ int	main(int argc, char **argv)
 		}
 	}
 	write(1, "\n", 1);
+	leaks();
 	return (0);
 }
