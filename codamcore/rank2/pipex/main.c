a0b272dc8480599e2d4de527b2ade041941427fd
Julia van Tol <juliavantol@live.nl>
2023-02-14 14:11:35 +0100

    add main

--- codamcore/rank2/pipex/main.c
+++ codamcore/rank2/pipex/main.c
@@ -6,32 +6,57 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/02 16:31:44 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/02/13 14:27:43 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/02/14 14:05:36 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+// fd[0] == read
+// fd[1] == write
+
+int	pipex(char *stdin, char *stdout, char *cmd1, char *cmd2)
+{
+	int		fd[2];
+	int		id;
+	char	*str;
+	int		test;
+
+	if (pipe(fd) == -1)
+	{
+		perror("An error ocurred with opening the pipe\n");
+		return (0);
+	}
+	id = fork();
+	if (id == 0)
+	{
+		close(fd[0]);
+		str = "Hello!\n";
+		write(fd[1], str, ft_strlen(str));
+		close(fd[1]);
+	}
+	else
+	{
+		close(fd[1]);
+		read(fd[0], &test, sizeof(int));
+		close(fd[0]);
+	}
+	printf("%s %s %s %s\n", stdin, cmd1, stdout, cmd2);
+	return (0);
+}
+
 int	main(int argc, char **argv)
 {
-	char	*stdin;
-	char	*stdout;
-	char	*cmd1;
-	char	*cmd2;
 	int		len;
 
 	if (argc == 5)
 	{
-		stdin = argv[1];
-		cmd1 = argv[2];
-		stdout = argv[3];
-		cmd2 = argv[4];
-		printf("%s %s %s %s\n", stdin, cmd1, stdout, cmd2);
+		pipex(argv[1], argv[3], argv[2], argv[4]);
 	}
 	else
 	{
 		len = ft_strlen("heyyy!");
 		ft_printf("%d\n", len);
-		return 0;
+		return (0);
 	}
 }
