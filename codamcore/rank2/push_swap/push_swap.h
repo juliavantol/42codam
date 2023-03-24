12c224cfe35892d386318571e458c41b98cb263b
Julia van Tol <juliavantol@live.nl>
2023-03-24 13:46:48 +0100

    add libft and header

---
+++ codamcore/rank2/push_swap/push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   push_swap.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/03/24 12:51:44 by juvan-to      #+#    #+#                 */
+/*   Updated: 2023/03/24 13:01:26 by juvan-to      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include "libft/libft.h"
+
+#endif
