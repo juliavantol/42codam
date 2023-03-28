09f809580f4238c46d90c19265ce998e99746b5a
Julia van Tol <juliavantol@live.nl>
2023-03-28 14:36:31 +0200

    adding struct

--- codamcore/rank2/push_swap/push_swap.c
+++ codamcore/rank2/push_swap/push_swap.c
@@ -6,19 +6,38 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/24 12:48:08 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/03/27 15:35:59 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/03/28 14:34:43 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	print_stack(t_node *stack)
+{
+	t_node	*temp;
+
+	temp = stack;
+	if (stack != NULL)
+	{
+		while (temp -> next != NULL)
+		{
+			printf("%d\n", temp -> value);
+			temp = temp -> next;
+		}
+	}
+}
+
+// atexit(leaks);
 int	main(int argc, char **argv)
 {
-	// atexit(leaks);
-	int	input[argc];
+	t_list	**stack_a;
+	t_list	*new;
 
 	if (argc < 2)
 		exit(1);
-	input[0] = 0;
 	check_input(argc, argv);
+	stack_a = (t_list **)malloc(sizeof(t_list));
+	*stack_a = NULL;
+	new = ft_lstnew((void *)1);
+	ft_lstadd_back(stack_a, new);
 }
--- codamcore/rank2/push_swap/push_swap.h
+++ codamcore/rank2/push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/24 12:51:44 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/03/27 15:36:49 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/03/28 14:17:23 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@
 # include "libft/libft.h"
 # include <stdlib.h>
 
+typedef struct s_node
+{
+	int				value;
+	int				index;
+	struct s_node	*next;
+}	t_node;
+
 void	leaks(void);
 int		ft_digit_str(char	*str);
 void	check_input(int argc, char	**argv);
-
 
 #endif
