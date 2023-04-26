9b571d908f8e6dff477eee1981bfdfd1ad228970
Julia van Tol <juliavantol@live.nl>
2023-04-26 20:30:38 +0200

    add so_long

---
+++ codamcore/rank2/so_long/so_long.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   so_long.h                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/04/26 15:30:42 by juvan-to      #+#    #+#                 */
+/*   Updated: 2023/04/26 15:48:34 by juvan-to      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "include/libft/libft.h"
+
+void	ft_error(char *str);
+
+#endif
