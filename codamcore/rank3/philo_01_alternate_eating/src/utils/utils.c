708347351801afeab0f0fb08cc154896deceb764
Julia van Tol <juliavantol@live.nl>
2023-07-26 17:28:53 +0200

    alternate eating

---
+++ codamcore/rank3/philo_01_alternate_eating/src/utils/utils.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   utils.c                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/12 11:55:21 by juvan-to      #+#    #+#                 */
+/*   Updated: 2023/07/26 14:27:14 by juvan-to      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	message(t_data *data, int state, int id)
+{
+	u_int64_t	time;
+
+	time = get_time_ms() - data->start_time;
+	pthread_mutex_lock(&data->write);
+	if (state == EATING)
+		printf("%llu %d is eating\n", time, id);
+	else if (state == SLEEPING)
+		printf("%llu %d is sleeping\n", time, id);
+	else if (state == THINKING)
+		printf("%llu %d is thinking\n", time, id);
+	else if (state == FORK)
+		printf("%llu %d has taken a fork\n", time, id);
+	pthread_mutex_unlock(&data->write);
+}
+
+u_int64_t	get_time_ms(void)
+{
+	struct timeval	tv;
+	u_int64_t		time;
+
+	gettimeofday(&tv, NULL);
+	time = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+	return (time);
+}
