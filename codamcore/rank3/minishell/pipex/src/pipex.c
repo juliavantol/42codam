3270dc3e4fac2a22e276c81b962abec43f9bc827
Julia van Tol <juliavantol@live.nl>
2023-09-01 23:17:09 +0200

    no double code

--- codamcore/rank3/minishell/pipex/src/pipex.c
+++ codamcore/rank3/minishell/pipex/src/pipex.c
@@ -6,36 +6,42 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/21 13:17:48 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/09/01 23:04:46 by Julia         ########   odam.nl         */
+/*   Updated: 2023/09/01 23:16:25 by Julia         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	output(char *output, char *cmd, t_pipex pipex)
+void	run_command(char *cmd, t_pipex pipex)
 {
 	char	*path;
 
-	pipex.outfile = open(output, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (pipex.outfile < 0)
-		error_exit("File couldn't be opened");
-	if (access(output, R_OK) != 0 || access(output, W_OK) != 0)
-		error_exit("Can't be read/written");
-	dup2(pipex.outfile, 1);
 	if (pipex.cmd_split != NULL)
 	{
 		path = get_cmd_path(pipex.paths, cmd);
 		if (!path)
 			error_exit("Command not found");
 		if (execve(path, pipex.cmd_split, pipex.full_envp) == -1)
+		{
+			ft_putstr_fd("Execve error", 2);
 			error_exit("Execve error");
+		}
 	}
+}
+
+void	output(char *output, char *cmd, t_pipex pipex)
+{
+	pipex.outfile = open(output, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (pipex.outfile < 0)
+		error_exit("File couldn't be opened");
+	if (access(output, R_OK) != 0 || access(output, W_OK) != 0)
+		error_exit("Can't be read/written");
+	dup2(pipex.outfile, 1);
+	run_command(cmd, pipex);
 }
 
 void	child_process(int fds[], t_pipex pipex)
 {
-	char	*path;
-
 	if (pipex.infile < 0)
 	{
 		ft_putstr_fd("pipex: input: No such file or directory\n", 2);
@@ -44,16 +50,7 @@ void	child_process(int fds[], t_pipex pipex)
 	close(fds[0]);
 	dup2(fds[1], 1);
 	if (pipex.cmd_split != NULL)
-	{
-		path = get_cmd_path(pipex.paths, pipex.cmd);
-		if (!path)
-			error_exit("Command not found");
-		if (execve(path, pipex.cmd_split, pipex.full_envp) == -1)
-		{
-			ft_putstr_fd("Execve error", 2);
-			exit(127);
-		}
-	}
+		run_command(pipex.cmd, pipex);
 }
 
 void	pipes(t_pipex pipex)
