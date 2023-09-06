81b16c3698f617544439a1f1567c1ac064b28ea1
Julia van Tol <juliavantol@live.nl>
2023-09-06 22:28:15 +0200

    no longer hardcoded lol

--- codamcore/rank3/minishell/pipex/src/heredoc.c
+++ codamcore/rank3/minishell/pipex/src/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: Julia <Julia@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/05 23:08:03 by Julia         #+#    #+#                 */
-/*   Updated: 2023/09/06 22:24:31 by Julia         ########   odam.nl         */
+/*   Updated: 2023/09/06 22:27:40 by Julia         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 void	here_doc(char *delimiter)
 {
 	char	*input;
+	size_t	delimiter_len;
 
+	delimiter_len = ft_strlen(delimiter);
 	while (1)
 	{
 		input = get_next_line(0);
-		if (ft_strnstr(input, delimiter, 4) && ft_strlen(input) == 5
-			&& input[4] == '\n')
+		if (ft_strnstr(input, delimiter, delimiter_len)
+			&& ft_strlen(input) == delimiter_len + 1 && input[4] == '\n')
 		{
 			return ;
 		}
