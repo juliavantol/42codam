1012a950b02a995a9346bbadb7e2283abbefde64
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2023-09-14 13:00:10 +0200

    back to char dup

--- rank3/minishell/include/minishell.h
+++ rank3/minishell/include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: Julia <Julia@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/09 23:09:51 by Julia         #+#    #+#                 */
-/*   Updated: 2023/09/12 14:28:10 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/09/14 12:59:52 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,16 @@
 # include <limits.h>
 # include "libft.h"
 
-typedef struct s_envp
-{
-	char			*variable_name;
-	char			*line;
-}	t_envp;
-
 typedef struct s_data
 {
-	t_envp	**envp;
+	char	**envp;
 }	t_data;
 
 bool	proper_start(int argc, char **argv);
 bool	valid_input(const char *input);
 char	*init_prompt(const char *prompt);
+char	**dup_envp(char **envp);
 void	execute(char *input, char **envp);
-
-
-t_envp	**dup_envp(char **envp);
 void	check_command(t_data *data, char *input);
 
 #endif
