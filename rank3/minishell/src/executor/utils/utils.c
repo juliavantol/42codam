86a3b192543c427b21e66231b594b9530f0e1050
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2023-09-15 17:23:47 +0200

    get rid of some strjoins

--- rank3/minishell/src/executor/utils/utils.c
+++ rank3/minishell/src/executor/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/21 13:29:24 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/09/11 11:50:46 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/09/15 17:23:11 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ char	*get_cmd_path(char **paths, char *cmd)
 	cmd = *ft_split(cmd, ' ');
 	while (*paths)
 	{
-		path = join_str(*paths, cmd);
+		path = join_three_strs(*paths, NULL, cmd);
 		if (access(path, F_OK) == 0)
 			return (free(cmd), path);
 		free(path);
@@ -59,7 +59,7 @@ char	**ft_split_paths(char *whole_str)
 		error_exit("Malloc error");
 	while (split_paths[index])
 	{
-		temp = join_str(split_paths[index], "/");
+		temp = join_three_strs(split_paths[index], NULL, "/");
 		free(split_paths[index]);
 		split_paths[index] = temp;
 		index++;
--- rank3/minishell/src/libft/ft_strjoin.c
+++ rank3/minishell/src/libft/ft_strjoin.c
@@ -6,56 +6,35 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/19 07:25:43 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/09/15 16:45:04 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/09/15 17:19:16 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*join_three_strs(char *s1, char *s2, char *s3)
 {
-	char	*new_str;
-	size_t	str_len;
-	int		i;
-	int		second;
-
-	str_len = ft_strlen(s1) + ft_strlen(s2);
-	new_str = (char *)ft_malloc((str_len + 1) * sizeof(char));
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		new_str[i] = s1[i];
-		i++;
-	}
-	second = i;
-	i = 0;
-	while (s2[i] != '\0')
-		new_str[second++] = s2[i++];
-	new_str[second] = '\0';
-	return (new_str);
-}
-
-char	*ft_strjoin_multiple(char **strs)
-{
-	char	*output;
-	int		count;
 	int		i;
 	int		j;
+	char	*output;
 
-	count = 0;
+	i = ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3);
+	output = ft_malloc(sizeof(char) * (i + 1));
 	i = 0;
-	while (strs[i])
-		count += ft_strlen(strs[i++]);
-	output = ft_malloc(count + 1 * sizeof(char));
-	i = 0;
-	count = 0;
-	while (strs[i])
+	j = 0;
+	while (s1[i])
 	{
-		j = 0;
-		while (strs[i][j])
-			output[count++] = strs[i][j++];
+		output[i] = s1[i];
 		i++;
 	}
-	output[count] = '\0';
+	if (s2)
+	{
+		while (s2[j])
+			output[i++] = s2[j++];
+	}
+	j = 0;
+	while (s3[j])
+		output[i++] = s3[j++];
+	output[i] = '\0';
 	return (output);
 }
