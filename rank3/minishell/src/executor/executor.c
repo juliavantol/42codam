9a55c7bae46b7da94c7f4be780d177daa1ff2315
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2023-10-02 13:53:44 +0200

    grep dont work

--- rank3/minishell/src/executor/executor.c
+++ rank3/minishell/src/executor/executor.c
@@ -6,34 +6,11 @@
 /*   By: Julia <Julia@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/31 02:27:35 by Julia         #+#    #+#                 */
-/*   Updated: 2023/10/02 13:05:49 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/10/02 13:52:05 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
-
-void	redirect_last_command(t_exe *executor, char **cmd)
-{
-	int		fds[2];
-	int		status;
-	pid_t	pid;
-
-	if (pipe(fds) < 0)
-		error_exit("Error with opening the pipe");
-	pid = fork();
-	if (pid < 0)
-		error_exit("Error with fork");
-	if (pid == 0)
-	{
-		dup2(OUTPUT, 1);
-		run_command(executor, cmd);
-		exit(EXIT_SUCCESS);
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-	}
-}
 
 void	run_command(t_exe *executor, char **split_cmd)
 {
@@ -48,48 +25,41 @@ void	run_command(t_exe *executor, char **split_cmd)
 	}
 }
 
-void	execute_multiple_command(t_exe *executor)
+void	execute_multiple_command(t_exe *executor, char **cmd, int index)
 {
 	int		fds[2];
 	int		status;
-	int		index;
-	char	**cmd;
 	pid_t	pid;
 
-	index = 0;
-	while (executor->commands[index])
+	if (index < (executor->command_count - 1))
+		pipe(fds);
+	pid = fork();
+	if (pid == 0)
 	{
-		cmd = ft_split(executor->commands[index], ' ');
+		if (index > 0)
+		{
+			dup2(fds[READ], INPUT);
+			close(fds[READ]);
+		}
 		if (index < (executor->command_count - 1))
-			pipe(fds);
-		pid = fork();
-		// child
-		if (pid == 0)
 		{
-			if (index > 0)
-			{
-				dup2(fds[READ], INPUT);
-				close(fds[READ]);
-			}
-			if (index < (executor->command_count - 1))
-			{
-				dup2(fds[WRITE], OUTPUT);
-				close(fds[WRITE]);
-			}
-			run_command(executor, cmd);
-			empty_array(cmd);
+			dup2(fds[WRITE], OUTPUT);
+			close(fds[WRITE]);
 		}
-		// parent
-		else
-		{
-			if (index > 0)
-				close(fds[READ]);
-			if (index < (executor->command_count - 1))
-				close(fds[WRITE]);
-			waitpid(pid, &status, 0);
-		}
-		index++;
+		run_command(executor, cmd);
+		exit(EXIT_FAILURE);
 	}
+	else
+	{
+		if (index > 0)
+			close(fds[READ]);
+		if (index < (executor->command_count - 1))
+			close(fds[WRITE]);
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status) != EXIT_SUCCESS)
+			exit(EXIT_FAILURE);
+	}
+	index++;
 }
 
 void	execute_single_command(t_exe *executor)
@@ -125,15 +95,10 @@ void	start_executor(t_exe *executor)
 		execute_single_command(executor);
 	else
 	{
-		execute_multiple_command(executor);
-		return ;
 		while (executor->commands[index])
 		{
 			cmd = ft_split(executor->commands[index], ' ');
-			if (index == executor->command_count - 1)
-				redirect_last_command(executor, cmd);
-			else
-				execute_multiple_command(executor);
+			execute_multiple_command(executor, cmd, index);
 			empty_array(cmd);
 			index++;
 		}
