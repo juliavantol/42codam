00bff07da5a98f0289712a798efbf197c62f7050
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2023-10-19 14:28:13 +0200

    save

--- rank3/minishell/src/executor/utils/set_envp.c
+++ rank3/minishell/src/executor/utils/set_envp.c
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/09/01 13:19:15 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/09/28 13:42:40 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/10/19 14:26:41 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,4 @@ char	**get_paths(char **envp)
 		return (NULL);
 	split_paths = ft_split_paths(paths);
 	return (split_paths);
-}
-
-char	**manual_envp(void)
-{
-	char		**paths;
-	const char	*temp[] = {"/usr/local/sbin/",
-		"/usr/local/bin/", "/usr/sbin/", "/usr/bin/", "/sbin/", "/bin/"};
-	int			index;
-
-	index = 0;
-	paths = ft_malloc(7 * sizeof(char *));
-	while (index < 6)
-	{
-		paths[index] = ft_strdup(temp[index]);
-		if (paths[index] == NULL)
-			error_exit("ft_malloc error");
-		index++;
-	}
-	paths[index] = NULL;
-	return (paths);
 }
