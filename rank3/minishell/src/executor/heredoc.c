d9052d4d3aa8532fddc658d361eb32dd3c544e1e
Julia van Tol <juliavantol@live.nl>
2023-11-05 23:24:49 +0100

    get rid of gnl

--- rank3/minishell/src/executor/heredoc.c
+++ rank3/minishell/src/executor/heredoc.c
@@ -6,73 +6,69 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/03 14:21:27 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/11/05 18:30:15 by Julia         ########   odam.nl         */
+/*   Updated: 2023/11/05 23:24:07 by Julia         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-// int	signal_received;
+int	signal_received;
 
-// static void	ft_here_sig(int signal)
-// {
-// 	if (signal == SIGINT)
-// 		exit(1);
-// }
+static void	ft_here_sig(int signal)
+{
+	if (signal == SIGINT)
+		exit(1);
+}
 
-// static void	ft_child_sig(int signal)
-// {
-// 	if (signal == SIGINT)
-// 		printf("\n");
-// }
+static void	ft_child_sig(int signal)
+{
+	if (signal == SIGINT)
+		printf("\n");
+}
 
-// void	heredoc_signal_handler(int signal_num)
-// {
-// 	signal_received = QUIT;
-// 	(void) signal_num;
-// }
+void	heredoc_signal_handler(int signal_num)
+{
+	signal_received = QUIT;
+	(void) signal_num;
+}
 
-// void	init_heredoc_signal_handler(void)
-// {
-// 	signal(SIGINT, heredoc_signal_handler);
-// 	signal(SIGQUIT, SIG_IGN);
-// }
+void	init_heredoc_signal_handler(void)
+{
+	signal(SIGINT, heredoc_signal_handler);
+	signal(SIGQUIT, SIG_IGN);
+}
 
-// void	fill_heredoc_file(char *content)
-// {
-// 	int		heredoc_file;
+void	fill_heredoc_file(void)
+{
+	int		heredoc_file;
 
-// 	heredoc_file = open_file(".here_doc", TRUNCATE);
-// 	write(heredoc_file, content, ft_strlen(content));
-// 	close(heredoc_file);
-// 	heredoc_file = open_file(".here_doc", READ);
-// 	dup2(heredoc_file, READ);
-// 	close(heredoc_file);
-// 	unlink(".here_doc");
-// }
+	heredoc_file = open_file(".here_doc", READ);
+	dup2(heredoc_file, READ);
+	close(heredoc_file);
+	unlink(".here_doc");
+}
 
-// void	here_doc2(t_filenames *head, char *delimiter)
-// {
-// 	char	*content;
-// 	char	*input;
-// 	int		len;
+void	here_doc(t_filenames *head, char *delimiter)
+{
+	char	*input;
+	int		heredoc_file;
 
-// 	len = ft_strlen(delimiter);
-// 	input = NULL;
-// 	content = "";
-// 	signal(SIGQUIT, &ft_child_sig);
-// 	signal(SIGINT, &ft_child_sig);
-// 	while (1)
-// 	{
-// 		signal(SIGQUIT, SIG_IGN);
-// 		signal(SIGINT, &ft_here_sig);
-// 		input = get_next_line(STDIN_FILENO);
-// 		if (ft_strnstr(input, delimiter, len) && input[len] == '\n')
-// 			break ;
-// 		content = join_three_strs(content, NULL, input);
-// 		free(input);
-// 	}
-// 	if (head->next == NULL)
-// 		fill_heredoc_file(content);
-// 	free(content);
-// }
+	input = NULL;
+	signal(SIGQUIT, &ft_child_sig);
+	signal(SIGINT, &ft_child_sig);
+	heredoc_file = open_file(".here_doc", TRUNCATE);
+	while (1)
+	{
+		signal(SIGQUIT, SIG_IGN);
+		signal(SIGINT, &ft_here_sig);
+		input = readline("> ");
+		if (ft_strcmp(input, delimiter))
+			break ;
+		input = join_three_strs(input, NULL, "\n");
+		write(heredoc_file, input, ft_strlen(input));
+		free(input);
+	}
+	close(heredoc_file);
+	if (head->next == NULL)
+		fill_heredoc_file();
+}
--- rank3/minishell/src/executor/new_heredoc.c
+++ rank3/minishell/src/executor/new_heredoc.c
@@ -1,49 +1,49 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   new_heredoc.c                                      :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: Julia <Julia@student.codam.nl>               +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/11/05 18:08:12 by Julia         #+#    #+#                 */
-/*   Updated: 2023/11/05 19:18:37 by Julia         ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        ::::::::            */
+// /*   new_heredoc.c                                      :+:    :+:            */
+// /*                                                     +:+                    */
+// /*   By: Julia <Julia@student.codam.nl>               +#+                     */
+// /*                                                   +#+                      */
+// /*   Created: 2023/11/05 18:08:12 by Julia         #+#    #+#                 */
+// /*   Updated: 2023/11/05 19:18:37 by Julia         ########   odam.nl         */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "executor.h"
+// #include "executor.h"
 
-static void	ft_here_sig(int signal)
-{
-	if (signal == SIGINT)
-	{
-		exit(1);
-	}
-}
+// static void	ft_here_sig(int signal)
+// {
+// 	if (signal == SIGINT)
+// 	{
+// 		exit(1);
+// 	}
+// }
 
-static void	ft_child_sig(int signal)
-{
-	if (signal == SIGINT)
-	{
-		printf("\n");
-	}
-}
+// static void	ft_child_sig(int signal)
+// {
+// 	if (signal == SIGINT)
+// 	{
+// 		printf("\n");
+// 	}
+// }
 
-void	here_doc(t_filenames *head, char *delimiter)
-{
-	char	*input;
+// void	here_doc(t_filenames *head, char *delimiter)
+// {
+// 	char	*input;
 
-	ignore_signals();
-	signal(SIGQUIT, &ft_child_sig);
-	signal(SIGINT, &ft_child_sig);
-	while (1)
-	{
-		signal(SIGQUIT, SIG_IGN);
-		signal(SIGINT, &ft_here_sig);
-		input = readline("> ");
-		if (ft_strcmp(input, delimiter))
-			break ;
-		free(input);
-	}
-	head--;
-	restore_signals();
-}
+// 	ignore_signals();
+// 	signal(SIGQUIT, &ft_child_sig);
+// 	signal(SIGINT, &ft_child_sig);
+// 	while (1)
+// 	{
+// 		signal(SIGQUIT, SIG_IGN);
+// 		signal(SIGINT, &ft_here_sig);
+// 		input = readline("> ");
+// 		if (ft_strcmp(input, delimiter))
+// 			break ;
+// 		free(input);
+// 	}
+// 	head--;
+// 	restore_signals();
+// }
