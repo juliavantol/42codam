36c31dd81402f090577c00f7ae02ec1f30b33987
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2023-11-29 15:31:08 +0100

    redir struct

--- rank3/minishell/src/parser/parser.c
+++ rank3/minishell/src/parser/parser.c
@@ -6,18 +6,28 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/29 14:25:24 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/11/29 15:18:04 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/11/29 15:30:26 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-bool	parse_redirection(t_tokens *token)
+bool	parse_redirection(t_tokens *token, t_cmd *cmd_node)
 {
-	if (token->type == INPUT_REDIRECTION || token->type == OUTPUT_REDIRECTION
-		|| token->type == APPEND_REDIRECTION || token->type == HEREDOC)
+	char	*filename;
+
+	if (token->type == OUTPUT_REDIRECTION || token->type == APPEND_REDIRECTION)
 	{
-		printf("%d\n", token->type);
+		filename = token->next->value;
+		add_redir(&cmd_node->outputs, filename, token->type);
+		cmd_node->output_redirection = true;
+		return (true);
+	}
+	else if (token->type == INPUT_REDIRECTION || token->type == HEREDOC)
+	{
+		filename = token->next->value;
+		add_redir(&cmd_node->inputs, filename, token->type);
+		cmd_node->input_redirection = true;
 		return (true);
 	}
 	return (false);
@@ -35,9 +45,14 @@ void	run_parser(t_tokens *tokens)
 	current_cmd = last_command(commands_list);
 	while (tokens)
 	{
-		parse_redirection(tokens);
-		if (tokens->type == PIPE)
-			add_command(&commands_list, index++);
-		tokens = tokens->next;
+		if (parse_redirection(tokens, current_cmd) == true)
+			tokens = tokens->next->next;
+		if (tokens)
+		{
+			printf("value: %s\n", tokens->value);
+			if (tokens->type == PIPE)
+				add_command(&commands_list, index++);
+			tokens = tokens->next;
+		}
 	}
 }
