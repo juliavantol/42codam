6b2671d7737218bbcd9d370f5becf6aed1181141
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2023-11-29 16:09:13 +0100

    nvm it works

--- rank3/minishell/src/parser/parser.c
+++ rank3/minishell/src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/29 14:25:24 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/11/29 16:06:17 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/11/29 16:08:55 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ void	run_parser(t_exe *executor, t_tokens *tokens)
 	int		index;
 
 	commands_list = NULL;
-	index = 0;
+	index = 1;
 	add_command(&commands_list, index);
 	current_cmd = last_command(commands_list);
 	while (tokens)
