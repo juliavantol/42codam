1a99fd51a21758b3568b2ece07580247e3dc9729
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2023-11-29 13:42:56 +0100

    working i think

--- rank3/minishell/src/builtins/export_utils.c
+++ rank3/minishell/src/builtins/export_utils.c
@@ -6,21 +6,36 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/29 12:41:24 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/11/29 12:46:31 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/11/29 13:42:19 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
+// export test1="eerste zin" test2="dit is de tweede zin"
+
 void	handle_key(t_exe *executor, char **split_key, char **value)
 {
+	// char	*temp;
+
 	if (ft_strlen(*value) > 0)
 	{
 		export(executor, split_key[0], *value);
 		free(*value);
 		*value = ft_strdup("");
 	}
+	// temp = *value;
+	// *value = join_three_strs(*value, " ", split_key[1]);
+	// free(temp);
+}
+
+void	handle_value(char **split_key, char **value)
+{
+	char	*temp;
+
+	temp = *value;
 	*value = join_three_strs(*value, " ", split_key[1]);
+	free(temp);
 }
 
 void	export_all_keys(t_exe *executor, char **cmd_split, int i)
@@ -33,14 +48,16 @@ void	export_all_keys(t_exe *executor, char **cmd_split, int i)
 	{
 		if (ft_strnstr(cmd_split[i], "=", ft_strlen(cmd_split[i])))
 		{
+			if (split_key != NULL)
+				handle_key(executor, split_key, &value);
 			split_key = ft_split(cmd_split[i], '=');
-			handle_key(executor, split_key, &value);
+			handle_value(split_key, &value);
 		}
 		else
 			value = join_three_strs(value, " ", cmd_split[i]);
 		i++;
 	}
-	if (ft_strlen(value) > 0)
+	if (split_key != NULL && ft_strlen(value) > 0)
 	{
 		export(executor, split_key[0], value);
 		free(value);
