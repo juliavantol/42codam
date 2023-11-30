beb7a9b4d78ba062bbe863a425b284d33ea006e1
Julia van Tol <juliavantol@live.nl>
2023-11-30 01:05:39 +0100

    save

--- rank3/minishell/src/signals/heredoc_signals.c
+++ rank3/minishell/src/signals/heredoc_signals.c
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/11/29 16:38:31 by juvan-to      #+#    #+#                 */
-/*   Updated: 2023/11/29 16:38:59 by juvan-to      ########   odam.nl         */
+/*   Updated: 2023/11/30 01:04:31 by Julia         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,14 @@ static void	heredoc_signal_handler(int signal)
 		printf("\n");
 		exit(130);
 	}
+	else if (signal == SIGQUIT)
+	{
+		exit(131);
+	}
 }
 
 void	init_heredoc_signal_handler(void)
 {
 	signal(SIGINT, heredoc_signal_handler);
+	signal(SIGQUIT, heredoc_signal_handler);
 }
