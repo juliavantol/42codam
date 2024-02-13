312fbfb0f718322de71cd6db9eff853285a9b7ff
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-02-13 13:40:35 +0100

    done ex02

--- rank4/cpp01/ex01/zombieHorde.cpp
+++ rank4/cpp01/ex01/zombieHorde.cpp
@@ -1,34 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   Zombie.cpp                                         :+:    :+:            */
+/*   zombieHorde.cpp                                    :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/02/13 12:07:03 by juvan-to      #+#    #+#                 */
-/*   Updated: 2024/02/13 13:26:15 by juvan-to      ########   odam.nl         */
+/*   Created: 2024/02/13 13:26:19 by juvan-to      #+#    #+#                 */
+/*   Updated: 2024/02/13 13:26:35 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(void)
+Zombie	*zombieHorde(int N, std::string name)
 {
-	std::cout << "A zombie has been constructed." << std::endl;
-	return;
-}
-Zombie::~Zombie(void)
-{
-	std::cout << this->_name << " has died." << std::endl;
-	return;
-}
+	if (N < 0)
+		return NULL;
+	
+	Zombie	*zombieHorde = new Zombie[N];
+	if (!zombieHorde)
+		return NULL;
+	
+	for (int i = 0; i < N; i++)
+		zombieHorde[i].init(name);
 
-void	Zombie::announce(void)
-{
-	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
-void	Zombie::init(std::string name)
-{
-	this->_name = name;
+	return zombieHorde;
 }
---
+++ rank4/cpp01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/02/13 13:27:51 by juvan-to      #+#    #+#                 */
+/*   Updated: 2024/02/13 13:40:15 by juvan-to      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "Memory address string: " << &string << std::endl;
+	std::cout << "Memory address stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address stringREF: " << &stringREF << std::endl << std::endl;
+
+	std::cout << "Value string: " << string << std::endl;
+	std::cout << "Value stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value stringREF: " << stringREF << std::endl;
+
+	return (0);
+
+}
