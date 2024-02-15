e85c4b622ffb5cb485d100a585f27d026c34c385
juliavantol <juliavantol@live.nl>
2024-02-15 02:41:37 +0100

    handles newlines

--- rank4/cpp01/ex04/main.cpp
+++ rank4/cpp01/ex04/main.cpp
@@ -6,30 +6,28 @@
 /*   By: Julia <Julia@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/14 01:31:21 by Julia         #+#    #+#                 */
-/*   Updated: 2024/02/14 16:04:08 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/02/15 02:40:05 by Julia         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
-std::string	replace_string(int len, std::string s1, std::string s2, std::string line)
+std::string	replace_string(std::string s1, std::string s2, std::string line)
 {
-	std::string		output;
-	std::string		temp;
-	size_t			found;
+	std::string		output = line;
+	size_t			found = line.find(s1);
 
-	output = line;
-	if (s1.empty() || s2.empty())
+	if (s1.empty())
 		return output;
-	found = line.find(s1);
 	while (found != std::string::npos)
 	{
-		temp = line.substr(found + len, std::string::npos);
-		output.erase(found, std::string::npos);
-		output.append(s2);
-		output.append(temp);
-		found = line.find(s1, found + len);
+		output.erase(found, s1.length());
+		if (s2.compare("\\n") == 0)
+			output.insert(found, "\n");
+		else
+			output.insert(found, s2);
+		found = output.find(s1, found + s1.length());
 	}
 	return output;
 }
@@ -39,20 +37,22 @@ int main(int argc, char **argv)
 	std::ofstream	output;
 	std::ifstream	input;
 	std::string		line;
-	char			*filename = argv[1];
 
 	if (argc != 4)
 	{
 		std::cout << "Invalid input" << std::endl;
 		return 0;
 	}
-	int len = std::string(argv[2]).length();
-	input.open(filename);
+	input.open(argv[1]);
 	output.open("output.txt");
 	if (input.is_open() && output.is_open())
 	{
 		while (getline(input, line))
-			output << replace_string(len, argv[2], argv[3], line) << std::endl;
+		{
+			output << replace_string(argv[2], argv[3], line);
+			if (std::string(argv[2]).compare("\\n") != 0)
+				output << std::endl;
+		}
 		input.close();
 		output.close();
 	}
