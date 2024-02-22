406122d843b30ee1ef7768ec76a2e202e99c03ad
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-02-22 14:56:39 +0100

    Changes to be committed:

--- rank4/cpp01/ex00/src/main.cpp
+++ rank4/cpp01/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/13 12:03:42 by juvan-to      #+#    #+#                 */
-/*   Updated: 2024/02/13 12:39:25 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/02/22 14:54:55 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 
 int main(void)
 {
+	// Heap allocated zombie
 	Zombie	*heap_zombie = newZombie("HeapZombie");
 	heap_zombie->announce();
-
+	
+	// Stack allocated zombie
 	randomChump("StackZombie");
 
+	// Deallocate heap allocated zombie
 	delete heap_zombie;
 	return 0;
 }
