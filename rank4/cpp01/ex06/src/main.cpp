9e96c8d936fff6d841baa5be906cab175751faaa
juliavantol <juliavantol@live.nl>
2024-02-27 22:57:58 +0100

    all checked

--- rank4/cpp01/ex06/src/main.cpp
+++ rank4/cpp01/ex06/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Julia <Julia@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/14 01:31:21 by Julia         #+#    #+#                 */
-/*   Updated: 2024/02/15 00:48:47 by Julia         ########   odam.nl         */
+/*   Updated: 2024/02/27 22:56:51 by Julia         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ int main(int argc, char **argv)
 	std::string	level;
 
 	if (argc != 2)
-		return 0;
+	{
+		std::cout << "Invalid input" << std::endl;
+		return 1;
+	}
 	level = argv[1];
 	customer.complain(level);
 	return 0;
