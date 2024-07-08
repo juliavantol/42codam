575ee928b081e2b47dfebfec6251b4853afa4e42
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-07-08 12:04:16 +0200

    better main

--- rank5/cpp05/ex00/src/Main.cpp
+++ rank5/cpp05/ex00/src/Main.cpp
@@ -6,7 +6,7 @@
 /*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/22 14:08:43 by juvan-to      #+#    #+#                 */
-/*   Updated: 2024/03/30 16:24:44 by juvan-to      ########   odam.nl         */
+/*   Updated: 2024/07/08 12:04:00 by juvan-to      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,27 @@
 
 int main(void)
 {
+	std::cout << "Constructing bureaucrat with 1 --> ";
 	try
 	{
-		Bureaucrat	high("B", 0);
+		Bureaucrat	high("A", 0);
 		std::cout << high.getName() << ": " << high.getGrade() << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << "Caught error: " << e.what() << std::endl;
 	}
+	std::cout << "Constructing bureaucrat with 151 --> ";
 	try
 	{
-		Bureaucrat	low("A", 151);
+		Bureaucrat	low("B", 151);
 		std::cout << low.getName() << ": " << low.getGrade() << std::endl;
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << "Caught error: " << e.what() << std::endl;
 	}
-	std::cout << std::endl;
+	std::cout << "Constructing bureaucrat with 100 --> ";
 	try
 	{
 		Bureaucrat	bob("Bob", 100);
@@ -43,6 +45,5 @@ int main(void)
 	{
 		std::cerr << "Caught error: " << e.what() << std::endl;
 	}
-	
 	return 0;
 }
