5a76be4dbf1f41061cff9ab34ead0496de2a78cf
Julia van Tol <juvan-to@f1r4s15.codam.nl>
2024-07-10 13:09:59 +0200

    init exo2

---
+++ rank5/cpp05/ex02/include/Bureaucrat.hpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   Bureaucrat.hpp                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/03/22 14:08:38 by juvan-to      #+#    #+#                 */
+/*   Updated: 2024/07/10 13:08:55 by juvan-to      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include "AForm.hpp"
+
+#define LOWEST_GRADE 	150
+#define HIGHEST_GRADE 	1
+#define GREEN			"\033[32m"
+#define BOLD			"\033[1m"
+#define RESET			"\033[0m"
+#define RED				"\033[31m"
+
+class AForm;
+
+class Bureaucrat
+{
+	private:
+		std::string	const	_name;
+		int					_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(Bureaucrat const &other);
+		Bureaucrat & operator=(Bureaucrat const &other);
+		~Bureaucrat();
+
+		std::string	getName(void) const;
+		int			getGrade(void) const;
+
+		void		incrementGrade(void);
+		void		decrementGrade(void);
+		void		signForm(AForm &form, bool status);
+
+		class GradeTooHighException: public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException: public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream & operator<<(std::ostream &o, const Bureaucrat &rhs);
+
+#endif
---
+++ rank5/cpp05/ex02/src/AForm.cpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   AForm.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: juvan-to <juvan-to@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/07/08 12:57:16 by juvan-to      #+#    #+#                 */
+/*   Updated: 2024/07/09 17:29:31 by juvan-to      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AForm.hpp"
+
+AForm::AForm(const std::string &name, int signGrade, int executeGrade) : _name(name), _signed(false)
+									, _signGrade(signGrade), _executeGrade(executeGrade)
+{
+	if (signGrade < HIGHEST_GRADE || executeGrade < HIGHEST_GRADE)
+		throw AForm::GradeTooHighException();
+	else if (signGrade > LOWEST_GRADE || executeGrade > LOWEST_GRADE)
+		throw AForm::GradeTooLowException();
+}
+
+AForm::~AForm(void)
+{
+	
+}
+
+// Copy constructor
+AForm::AForm(const AForm &other) : _name(other._name), _signed(other._signed)
+							, _signGrade(other._signGrade), _executeGrade(other._executeGrade) {
+}
+
+// Copy assignment operator
+AForm & AForm::operator=(const AForm &other)
+{
+	if (this != &other)
+	{
+		// Only non-const members can be assigned
+		this->_signed = other.getSignStatus();
+	}
+	return *this;
+}
+
+// Getters
+std::string	AForm::getName(void) const
+{
+	return _name;
+}
+
+bool	AForm::getSignStatus(void) const
+{
+	return _signed;
+}
+
+int		AForm::getSignGrade(void) const
+{
+	return _signGrade;
+}
+
+int		AForm::getExecuteGrade(void) const
+{
+	return _executeGrade;
+}
+
+// Sets the AForm status to signed if the bureaucrat’s grade is high enough
+void	AForm::beSigned(Bureaucrat &bureaucrat)
+{
+	if (bureaucrat.getGrade() <= this->getSignGrade())
+	{
+		this->_signed = true;
+		bureaucrat.signForm(*this, true);
+	}
+	else
+	{
+		this->_signed = false;
+		bureaucrat.signForm(*this, false);
+		throw AForm::GradeTooLowException();
+	}
+}
+
+// Exceptions
+const char*	AForm::GradeTooHighException::what() const throw()
+{
+	return "Grade too high";
+}
+
+const char*	AForm::GradeTooLowException::what() const throw()
+{
+	return "Grade too low";
+}
+
+// Overloading of the insertion operator
+std::ostream& operator<<(std::ostream &o, const AForm &rhs)
+{
+	o << "AForm name:		" << rhs.getName() << std::endl
+		<< "Sign status:		" << rhs.getSignStatus()
+		<< std::endl << "Grade to sign:		" << rhs.getSignGrade()
+		<< std::endl << "Grade to execute:	" << rhs.getExecuteGrade();
+	return o;
+}
